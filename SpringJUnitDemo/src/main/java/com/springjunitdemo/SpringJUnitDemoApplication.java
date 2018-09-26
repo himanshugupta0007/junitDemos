7f317adac485dc514c33beeb772e318da3187079
himanshugupta0007 <himanshugupta0007@gmail.com>
2018-09-26 11:42:12 +0530

    Junit Demo Project

---
+++ SpringJUnitDemo/src/main/java/com/springjunitdemo/SpringJUnitDemoApplication.java
@@ -0,0 +1,12 @@
+package com.springjunitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJUnitDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJUnitDemoApplication.class, args);
+	}
+}
